6b8231719dbb8fe3d425f83885c925119b98659f
Stuard Quiroz <stuuard96@gmail.com>
2022-11-05 13:42:32 -0500

    consultar a la base de datos prisma

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+export default function Home({ categorias }) {
   return <h1 className="title">NextJS</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany();
+  return {
+    props: {
+      categorias,
+    },
+  };
+};
